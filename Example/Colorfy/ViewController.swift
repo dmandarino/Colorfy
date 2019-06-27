58bb6a5eab9bea5167c6b30e1428dcf43f4f7b02
Douglas Mandarino <douglasmandarino@Douglas-MacBook-Pro.local>
2019-06-27 11:49:55 -0300

    Using others colors

--- Example/Colorfy/ViewController.swift
+++ Example/Colorfy/ViewController.swift
@@ -24,14 +24,14 @@ class ViewController: UIViewController {
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
             Colorfy.shared.configure(with: self.url)
         })
-        
+    
         self.firstView.backgroundColor = Colorfy.shared.color(named: "emerald") 
         
         Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: {_ in
             DispatchQueue.main.async {
                 self.view.backgroundColor = Colorfy.shared.color(named: "colorUi40")
-                self.secondView.backgroundColor = Colorfy.shared.color(named: "ui7")
-                self.label.textColor = Colorfy.shared.color(named: "ui4")
+                self.secondView.backgroundColor = Colorfy.shared.color(named: "colorUi60")
+                self.label.textColor = Colorfy.shared.color(named: "vermillion")
             }
         })
     }
