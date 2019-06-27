535527065bdeda7a4f3027beb828b875fd097ef7
Jonathan Bijos <jonathan.bijos@altran.com.br>
2019-06-27 11:42:18 -0300

    Add new buttons

--- Example/Colorfy/ViewController.swift
+++ Example/Colorfy/ViewController.swift
@@ -14,24 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstView: UIView!
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var lgbtButton: UIButton!
+    @IBOutlet weak var pinkOctoberButton: UIButton!
     
 //    private let url = URL(string: "https://api.myjson.com/bins/qj0p2")!
     private let url = URL(string: "https://api.myjson.com/bins/1heb86")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButtons()
+        setupLGBTGradient()
         
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
             Colorfy.shared.configure(with: self.url)
         })
-        
+    
         self.firstView.backgroundColor = Colorfy.shared.color(named: "emerald") 
         
         Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: {_ in
             DispatchQueue.main.async {
                 self.view.backgroundColor = Colorfy.shared.color(named: "colorUi40")
-                self.secondView.backgroundColor = Colorfy.shared.color(named: "ui7")
-                self.label.textColor = Colorfy.shared.color(named: "ui4")
+                self.secondView.backgroundColor = Colorfy.shared.color(named: "colorUi60")
+                self.label.textColor = Colorfy.shared.color(named: "vermillion")
             }
         })
     }
@@ -41,5 +45,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapLGBTButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapPinkOctoberButton(_ sender: UIButton) {
+        
+    }
+    
+    private func setupButtons() {
+        lgbtButton.layer.cornerRadius = lgbtButton.frame.size.height / 2
+        lgbtButton.layer.borderColor = Colorfy.shared.color(named: "white").cgColor
+        lgbtButton.layer.borderWidth = 2
+        
+        pinkOctoberButton.layer.cornerRadius = pinkOctoberButton.frame.size.height / 2
+        pinkOctoberButton.layer.borderColor = Colorfy.shared.color(named: "white").cgColor
+        pinkOctoberButton.layer.borderWidth = 2
+    }
+    
+    private func setupLGBTGradient() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [
+            Colorfy.shared.color(named: "lgbtRed").cgColor,
+            Colorfy.shared.color(named: "lgbtOrange").cgColor,
+            Colorfy.shared.color(named: "lgbtYellow").cgColor,
+            Colorfy.shared.color(named: "lgbtGreen").cgColor,
+            Colorfy.shared.color(named: "lgbtBlue").cgColor,
+            Colorfy.shared.color(named: "lgbtPurple").cgColor
+        ]
+        gradient.frame = lgbtButton.bounds
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.cornerRadius = lgbtButton.layer.cornerRadius
+        lgbtButton.layer.insertSublayer(gradient, at: 0)
+        lgbtButton.setTitleColor(Colorfy.shared.color(named: "white"), for: .normal)
+    }
 }
 
---
+++ Example/build/Pods.build/Debug-iphonesimulator/Colorfy.build/DerivedSources/Colorfy_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ColorfyVersionString[];
+ extern const double ColorfyVersionNumber;
+
+ const unsigned char ColorfyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Colorfy  PROJECT:Pods-1" "\n";
+ const double ColorfyVersionNumber __attribute__ ((used)) = (double)1.;
